d1bee6fc4dcf9de7a8f7379a9f58b9fd200b74cf
Peterson Fonseca <petersonjf@gmail.com>
2022-12-12 12:59:24 -0300

    tarefas criadas

--- src/components/HearderContent.tsx
+++ src/components/HearderContent.tsx
@@ -2,7 +2,7 @@ import styles from './HeaderContent.module.css'
 export function HeaderContent({ checked, todoList }) {
   return (
     <div className={styles.wrapper}>
-      <p className={styles.pLeft}>Tarefas criadas<span>5</span></p>
+      <p className={styles.pLeft}>Tarefas criadas<span>{todoList.length}</span></p>
       <p className={styles.pRight}>Concluídas<span>{checked.length} de {todoList.length}</span></p>
     </div>
   )
